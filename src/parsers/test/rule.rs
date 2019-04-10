b3d8d84145d3a14653da6b6a3533d1c06f2bee1c
bobtwinkles <srkoser+GitHub@gmail.com>
2019-04-10 16:38:59 -0400

    Add more assertions to simple rule test

--- src/parsers/test/rule.rs
+++ src/parsers/test/rule.rs
@@ -5,6 +5,8 @@ use crate::Engine;
 
 #[test]
 fn simple_rule() {
+    crate::test::setup();
+
     let block = create_span(
         r#"
 a: b
@@ -29,4 +31,13 @@ a: b
     let (i, _) = assert_ok!(parse_state.parse_line(i, &mut names, &mut engine));
 
     assert_complete!(i);
+
+    parse_state.close_rule(&mut engine);
+
+    let target_name = names.file_name("a").expect("Should have target name");
+    let rule = engine.database.get_rule(target_name).expect("Should have generated rule");
+    let dep_name = names.file_name("b").expect("Should have dep name");
+
+    assert!(rule.deps.len() == 1);
+    assert!(rule.deps.contains(&dep_name));
 }
