73f6b3d5d413b3945bbecb354bb7807c66614230
bobtwinkles <srkoser+GitHub@gmail.com>
2019-04-11 20:48:39 -0400

    Abstract variable_set_to in integration tests

--- tests/include.rs
+++ tests/include.rs
@@ -1,33 +1,11 @@
 #[macro_use]
 extern crate log;
 
+#[macro_use]
+mod util_macros;
+
 use hastur::{NameCache, Engine};
 use std::fs::File;
-
-macro_rules! variable_set_to (
-    ($names:expr, $engine:expr, $variable_name:expr, $value:expr) => {{
-        let names = &mut $names;
-        let engine = &$engine;
-        let variable_name = $variable_name;
-        let value = $value;
-
-        let variable_name = names
-            .variable_name(variable_name)
-            .expect(&format!(
-                "Variable named {:?} should have been interned",
-                variable_name
-            ));
-
-        assert_eq!(
-            &engine
-                .database
-                .get_variable(variable_name)
-                .expect(&format!("Variable named {:?} should have a value", variable_name))
-                .expand(names, &engine.database).1,
-            value
-        )
-    }}
-);
 
 #[test]
 fn simple_includes() {
