ced666660f23ba0a9d0094b56dbdadf8c289df16
bobtwinkles <srkoser+GitHub@gmail.com>
2019-04-19 16:30:02 -0400

    Implement a more sophisticated tokenizer
    This one recognizes keywords

---
+++ src/tokenizer/gen_text_matcher.py
@@ -0,0 +1,231 @@
+# Generates a Rust file which can parse the various directives and builtin
+# functions
+
+import os
+
+directives = {
+    "-include": "Include(IsSoft::Yes)",
+    "-load": "Load(IsSoft::Yes)",
+    "export": "Export",
+    "include": "Include(IsSoft::No)",
+    "load": "Load(IsSoft::No)",
+    "sinclude": "Include(IsSoft::Yes)",
+    "unexport": "UnExport",
+    "vpath": "VPath",
+
+    "else": "Else",
+    "endif": "EndIf",
+    "ifdef": "IfDef",
+    "ifeq": "IfEq",
+    "ifndef": "IfNDef",
+    "ifneq": "IfNeq",
+
+    "define": "Define",
+    "enddef": "Enddef",
+}
+
+for (k,v) in directives.items():
+    directives[k] = 'TokenType::Directive(Directive::%s)' % v
+
+functions = {
+    "abspath": "Abspath",
+    "addprefix": "AddPrefix",
+    "addsuffix": "AddSuffix",
+    "and": "And",
+    "basename": "BaseName",
+    "call": "Call",
+    "dir": "Dir",
+    "error": "Error",
+    "eval": "Eval",
+    "file": "File",
+    "filter": "Filter",
+    "filterout": "FilterOut",
+    "findstring": "FindString",
+    "firstword": "FirstWord",
+    "flavor": "Flavor",
+    "if": "If",
+    "info": "Info",
+    "join": "Join",
+    "lastword": "LastWord",
+    "notdir": "NotDir",
+    "or": "Or",
+    "origin": "Origin",
+    "patsubst": "PatSubst",
+    "realpath": "Realpath",
+    "sort": "Sort",
+    "strip": "Strip",
+    "subst": "Subst",
+    "suffix": "Suffix",
+    "value": "Value",
+    "warning": "Warning",
+    "wildcard": "Wildcard",
+    "word": "Word",
+    "wordlist": "WordList",
+    "words": "Words",
+}
+
+for (k,v) in functions.items():
+    functions[k] = 'TokenType::BuiltinFunction(BuiltinFunction::%s)' % v
+
+# These are special because they don't need whitespace directly after them
+custom_end_matcher = {
+}
+
+
+def build_rule_tree(d, init_rules):
+    value_tree = init_rules
+    if value_tree is None:
+        value_tree = dict()
+
+    for token in d.keys():
+        parent_tree = value_tree;
+        for c in token:
+            if c not in parent_tree:
+                parent_tree[c] = {}
+            parent_tree = parent_tree[c]
+        parent_tree['$'] = d[token]
+    return value_tree
+
+def collect_tokens(d):
+    chars = set()
+    for k in d.keys():
+        for c in k:
+            chars.add(c)
+    return chars
+
+# This defines a rust function that returns an Option<TokenType>,
+# where the Some() variant represents matching a specific token type and a None
+# represents a lack of a match. The paired usize is the end position of the
+# last character consumed, which may be the same as the start if the matcher doesn't match
+char_match_header = """
+#[inline]
+fn match{path}<IT> (last_end: usize, it: &mut Peekable<IT>) -> (usize, Option<TokenType>)
+  where IT: Iterator<Item = (usize, char)>
+{{
+    if let Some((_, chr)) = it.peek() {{
+        match chr.to_lowercase().next() {{"""
+
+match_char_template = """
+            Some('{char}') => {{
+                let (start, chr) = it.next().unwrap();
+                match{path}{path_char}(start + chr.len_utf8(), it)
+            }}"""
+
+match_whitespace_template = """
+            Some(c) if c.is_whitespace() => {{
+                (last_end, Some({token}))
+            }}
+            None => {{
+                (last_end, Some({token}))
+            }}
+            """
+
+match_custom_template = """
+            _ => {{
+                {custom}(last_end, it);
+            }}
+"""
+
+match_catchall_template = """
+            _ => (last_end, None)"""
+
+char_match_footer = """
+        }}
+    }} else {{
+        (last_end, None)
+    }}
+}}
+"""
+
+def gen_matcher(path, next_level):
+    output = char_match_header.format(path=path)
+
+    # Generate the match body
+    for (k, v) in next_level.items():
+        if k == '$':
+            continue
+        else:
+            path_char = k
+            if k == '-':
+                path_char = 'dash_'
+            output += match_char_template.format(path=path, path_char=path_char, char=k)
+
+    if '$' in next_level:
+        v = next_level['$']
+        if v in custom_end_matcher:
+            output += match_custom_template.format(custom=custom_end_matcher[v])
+        else:
+            output += match_whitespace_template.format(token=v)
+            output += match_catchall_template
+    else:
+        output += match_catchall_template
+
+    output += char_match_footer.format()
+    return output
+
+# Get the name of the rule for the given output expression
+def gen_final(output_expr):
+    rule_name = output_expr[1]
+    print('Generating output for rule', rule_name)
+
+    return ''
+
+def gen_rust_fns(rule_tree, path=None):
+    output = ""
+
+    if path is None:
+        # This is the root, generate the special root function
+        print('Doing root gen')
+        output = "pub fn do_match<IT>(last_end: usize, first_char: char, it: &mut Peekable<IT>) -> (usize, Option<TokenType>)\n"
+        output += "  where IT: Iterator<Item = (usize, char)>\n"
+        output += "{\n"
+        output += "    match first_char {\n"
+
+        for (k, v) in rule_tree.items():
+            assert k != '$'
+            if k == '-':
+                output += ' ' * 8 + "'-' => match_dash_(last_end, it),\n"
+            else:
+                output += ' ' * 8 + "'{char}' => match_{char}(last_end, it),\n".format(char=k)
+
+        output += ' ' * 8 + "_ => (last_end, None)\n"
+
+        output += "    }\n"
+        output += "}\n"
+
+        # And set up the initial path
+        path = "_"
+
+    for c in rule_tree.keys():
+        new_path = path + c
+        if c == '-':
+            new_path = path + "dash_"
+        if c == '$':
+            print('Hit terminal for', rule_tree[c])
+        else:
+            output += gen_matcher(new_path, rule_tree[c])
+            output += gen_rust_fns(rule_tree[c], new_path)
+    return output
+
+header_block = """
+//! DO NOT EDIT THIS MODULE BY HAND.
+//! It is automatically generated by the sibling python script, `{fname}`.
+
+use super::*;
+"""
+
+def main():
+    rule_tree = build_rule_tree(directives, None)
+    rule_tree = build_rule_tree(functions, rule_tree)
+
+    my_realpath = os.path.realpath(__file__)
+    my_path = os.path.dirname(my_realpath)
+    my_name = os.path.basename(my_realpath)
+    output_path = os.path.join(my_path, "text_matcher.rs")
+
+    with open(output_path, 'w') as outf:
+        outf.write(header_block.format(fname=my_name))
+        outf.write(gen_rust_fns(rule_tree))
+
+if __name__ == '__main__':
+    main()
---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,488 @@
+//! Implements tokenization of the makefile
+use std::iter::{Iterator, Peekable};
+
+// mod tokenizer_grammar;
+mod text_matcher;
+
+#[cfg(test)]
+use proptest_derive::Arbitrary;
+
+/// A token in a makefile
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Token {
+    /// Offset from the start of parsing, as defined by whatever iterator was
+    /// passed into `iterator_to_token_stream`
+    pub start: usize,
+    /// The type of this token
+    pub token_type: TokenType,
+    /// Final character
+    pub end: usize,
+}
+
+impl Token {
+    fn new(start: usize, token_type: TokenType, end: usize) -> Token {
+        Token {
+            start,
+            token_type,
+            end,
+        }
+    }
+}
+
+/// Represents the type of a token. All lengths are in bytes, not characters.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum TokenType {
+    /// A directive
+    Directive(Directive),
+    /// A builtin function name
+    BuiltinFunction(BuiltinFunction),
+    /// Some plain text
+    Text,
+    /// Whitespace characters
+    Whitespace,
+    /// An escaped character (character preceded by a \)
+    EscapedCharacter(Option<char>),
+    /// A variable assignment operator
+    VariableAssign(VariableAssign),
+    /// The start of a variable reference (a `$` token)
+    VariableReference,
+    /// An open parenthesis
+    OpenParen,
+    /// A close parenthesis
+    CloseParen,
+    /// An open brace `{`
+    OpenBrace,
+    /// A close brace `}`
+    CloseBrace,
+    /// A colon (or double colon)
+    Colon(IsDoubleColon),
+    /// A semicolon `;`
+    SemiColon,
+    /// A percent `%`
+    Percent,
+    /// An unescaped newline
+    NewLine,
+    /// A `#` character
+    CommentStart,
+}
+
+/// Is the colon involved a double colon?
+#[allow(missing_docs)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub enum IsDoubleColon {
+    No,
+    Yes,
+}
+
+/// Is the directive involved soft (i.e. are failures hard errors)?
+#[allow(missing_docs)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub enum IsSoft {
+    No,
+    Yes,
+}
+
+/// A type of variable assignment
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub enum VariableAssign {
+    /// Corresponds to `=`
+    Recursive,
+    /// Corresponds to `:=`
+    Simple(IsDoubleColon),
+    /// Corresponds to `+=`
+    Append,
+    /// Corresponds to `?=`
+    Conditional,
+    /// Corresponds to `!=`
+    Bang,
+}
+
+/// A builtin function
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[allow(missing_docs)]
+pub enum BuiltinFunction {
+    Abspath,
+    AddPrefix,
+    AddSuffix,
+    And,
+    BaseName,
+    Call,
+    Dir,
+    Error,
+    Eval,
+    File,
+    Filter,
+    FilterOut,
+    FindString,
+    FirstWord,
+    Flavor,
+    If,
+    Info,
+    Join,
+    LastWord,
+    NotDir,
+    Or,
+    Origin,
+    PatSubst,
+    Realpath,
+    Sort,
+    Strip,
+    Subst,
+    Suffix,
+    Value,
+    Warning,
+    Wildcard,
+    Word,
+    WordList,
+    Words,
+}
+
+/// All the directives that may occur
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[allow(missing_docs)]
+pub enum Directive {
+    Export,
+    Include(IsSoft),
+    Load(IsSoft),
+    UnExport,
+    VPath,
+
+    Else,
+    EndIf,
+    IfDef,
+    IfEq,
+    IfNDef,
+    IfNeq,
+
+    Define,
+    Enddef,
+}
+
+/// Internal iterator implementation
+struct TokenStream<IT> {
+    internal: IT,
+}
+
+const SPECIAL_CHARACTERS: &'static str = "\\$(){}:;%+?!=#";
+
+impl<IT> Iterator for TokenStream<Peekable<IT>>
+where
+    IT: Iterator<Item = (usize, char)>,
+{
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        let (start_idx, chr) = self.internal.next()?;
+        let mut end_idx = start_idx + chr.len_utf8();
+
+        macro_rules! consume_next {
+            () => {
+                match self.internal.next() {
+                    Some((idx, chr)) => {
+                        end_idx = idx + chr.len_utf8();
+                        Some(chr)
+                    }
+                    None => None,
+                }
+            };
+        };
+
+        macro_rules! token {
+            ($ty:expr) => {
+                Some(Token {
+                    start: start_idx,
+                    end: end_idx,
+                    token_type: $ty,
+                })
+            };
+        };
+
+        macro_rules! check_next {
+            ($chr:expr, $matches:expr, $nomatch:expr) => {{
+                if let Some((_, chr)) = self.internal.peek() {
+                    if *chr == $chr {
+                        consume_next!();
+
+                        $matches
+                    } else {
+                        $nomatch
+                    }
+                } else {
+                    $nomatch
+                }
+            }};
+        }
+
+        if chr == ' ' || chr == '\t' {
+            // Make only considers ' ' and '\t' to be whitespace. All other
+            // whitespace characters are interpreted as newlines.
+            while let Some((_next_idx, chr)) = self.internal.peek() {
+                let chr = *chr;
+                if chr != ' ' && chr != '\t' {
+                    break;
+                }
+                consume_next!();
+            }
+            token!(TokenType::Whitespace)
+        } else if chr.is_whitespace() {
+            token!(TokenType::NewLine)
+        } else if chr == '\\' {
+            match consume_next!() {
+                None => token!(TokenType::EscapedCharacter(None)),
+                Some(chr2) => token!(TokenType::EscapedCharacter(Some(chr2))),
+            }
+        } else if chr == '$' {
+            token!(TokenType::VariableReference)
+        } else if chr == '(' {
+            token!(TokenType::OpenParen)
+        } else if chr == ')' {
+            token!(TokenType::CloseParen)
+        } else if chr == '{' {
+            token!(TokenType::OpenBrace)
+        } else if chr == '}' {
+            token!(TokenType::CloseBrace)
+        } else if chr == ':' {
+            // This is tricky, since the token may be one of the following:
+            //  :  :: := ::=
+
+            let dcolon = check_next!(':', IsDoubleColon::Yes, IsDoubleColon::No);
+
+            let ty = check_next!(
+                '=',
+                TokenType::VariableAssign(VariableAssign::Simple(dcolon)),
+                TokenType::Colon(dcolon)
+            );
+            token!(ty)
+        } else if chr == ';' {
+            token!(TokenType::SemiColon)
+        } else if chr == '%' {
+            token!(TokenType::Percent)
+        } else if chr == '+' {
+            let ty = check_next!(
+                '=',
+                TokenType::VariableAssign(VariableAssign::Append),
+                TokenType::Text
+            );
+            token!(ty)
+        } else if chr == '?' {
+            let ty = check_next!(
+                '=',
+                TokenType::VariableAssign(VariableAssign::Conditional),
+                TokenType::Text
+            );
+            token!(ty)
+        } else if chr == '!' {
+            let ty = check_next!(
+                '=',
+                TokenType::VariableAssign(VariableAssign::Bang),
+                TokenType::Text
+            );
+            token!(ty)
+        } else if chr == '=' {
+            token!(TokenType::VariableAssign(VariableAssign::Recursive))
+        } else if chr == '#' {
+            token!(TokenType::CommentStart)
+        } else {
+            // Attempt to match using the generated matcher
+            let (updated_end, matched) = text_matcher::do_match(end_idx, chr, &mut self.internal);
+            end_idx = updated_end;
+            if matched.is_some() {
+                return token!(matched.unwrap());
+            }
+            while let Some((_next_idx, chr2)) = self.internal.peek() {
+                if chr2.is_whitespace() || SPECIAL_CHARACTERS.find(*chr2).is_some() {
+                    break;
+                }
+                consume_next!();
+            }
+            return token!(TokenType::Text);
+        }
+    }
+}
+
+/// Adapt an iterator over characters to an iterator over tokens
+pub fn iterator_to_token_stream(
+    it: impl Iterator<Item = (usize, char)>,
+) -> impl Iterator<Item = Token> {
+    TokenStream {
+        internal: it.peekable(),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    fn flatten_stream(original_string: &str, tokens: &[Token]) -> String {
+        let mut buffer = String::with_capacity(tokens.len());
+
+        for token in tokens {
+            match token.token_type {
+                TokenType::Whitespace => {
+                    let slice = &original_string[token.start..token.end];
+                    for chr in slice.chars() {
+                        assert!(chr.is_whitespace())
+                    }
+                    buffer.push_str(slice);
+                }
+                TokenType::Text => {
+                    let slice = &original_string[token.start..token.end];
+                    buffer.push_str(slice);
+                }
+                TokenType::EscapedCharacter(Some(c)) => {
+                    buffer.push('\\');
+                    buffer.push(c)
+                }
+                TokenType::Directive(directive) => match directive {
+                    Directive::Export => buffer.push_str("Export"),
+                    Directive::Include(soft) => match soft {
+                        IsSoft::Yes => buffer.push_str("-include"),
+                        IsSoft::No => buffer.push_str("include"),
+                    },
+                    Directive::Load(soft) => match soft {
+                        IsSoft::Yes => buffer.push_str("-load"),
+                        IsSoft::No => buffer.push_str("load"),
+                    },
+                    Directive::UnExport => buffer.push_str("UnExport"),
+                    Directive::VPath => buffer.push_str("VPath"),
+
+                    Directive::Else => buffer.push_str("Else"),
+                    Directive::EndIf => buffer.push_str("EndIf"),
+                    Directive::IfDef => buffer.push_str("IfDef"),
+                    Directive::IfEq => buffer.push_str("IfEq"),
+                    Directive::IfNDef => buffer.push_str("IfNDef"),
+                    Directive::IfNeq => buffer.push_str("IfNeq"),
+
+                    Directive::Define => buffer.push_str("Define"),
+                    Directive::Enddef => buffer.push_str("Enddef"),
+                },
+                TokenType::BuiltinFunction(function) => match function {
+                    BuiltinFunction::Abspath => buffer.push_str("Abspath"),
+                    BuiltinFunction::AddPrefix => buffer.push_str("AddPrefix"),
+                    BuiltinFunction::AddSuffix => buffer.push_str("AddSuffix"),
+                    BuiltinFunction::And => buffer.push_str("And"),
+                    BuiltinFunction::BaseName => buffer.push_str("BaseName"),
+                    BuiltinFunction::Call => buffer.push_str("Call"),
+                    BuiltinFunction::Dir => buffer.push_str("Dir"),
+                    BuiltinFunction::Error => buffer.push_str("Error"),
+                    BuiltinFunction::Eval => buffer.push_str("Eval"),
+                    BuiltinFunction::File => buffer.push_str("File"),
+                    BuiltinFunction::Filter => buffer.push_str("Filter"),
+                    BuiltinFunction::FilterOut => buffer.push_str("FilterOut"),
+                    BuiltinFunction::FindString => buffer.push_str("FindString"),
+                    BuiltinFunction::FirstWord => buffer.push_str("FirstWord"),
+                    BuiltinFunction::Flavor => buffer.push_str("Flavor"),
+                    BuiltinFunction::If => buffer.push_str("If"),
+                    BuiltinFunction::Info => buffer.push_str("Info"),
+                    BuiltinFunction::Join => buffer.push_str("Join"),
+                    BuiltinFunction::LastWord => buffer.push_str("LastWord"),
+                    BuiltinFunction::NotDir => buffer.push_str("NotDir"),
+                    BuiltinFunction::Or => buffer.push_str("Or"),
+                    BuiltinFunction::Origin => buffer.push_str("Origin"),
+                    BuiltinFunction::PatSubst => buffer.push_str("PatSubst"),
+                    BuiltinFunction::Realpath => buffer.push_str("Realpath"),
+                    BuiltinFunction::Sort => buffer.push_str("Sort"),
+                    BuiltinFunction::Strip => buffer.push_str("Strip"),
+                    BuiltinFunction::Subst => buffer.push_str("Subst"),
+                    BuiltinFunction::Suffix => buffer.push_str("Suffix"),
+                    BuiltinFunction::Value => buffer.push_str("Value"),
+                    BuiltinFunction::Warning => buffer.push_str("Warning"),
+                    BuiltinFunction::Wildcard => buffer.push_str("Wildcard"),
+                    BuiltinFunction::Word => buffer.push_str("Word"),
+                    BuiltinFunction::WordList => buffer.push_str("WordList"),
+                    BuiltinFunction::Words => buffer.push_str("Words"),
+                },
+                TokenType::EscapedCharacter(None) => buffer.push('\\'),
+                TokenType::VariableAssign(VariableAssign::Recursive) => buffer.push_str("="),
+                TokenType::VariableAssign(VariableAssign::Simple(IsDoubleColon::No)) => {
+                    buffer.push_str(":=")
+                }
+                TokenType::VariableAssign(VariableAssign::Simple(IsDoubleColon::Yes)) => {
+                    buffer.push_str("::=")
+                }
+                TokenType::VariableAssign(VariableAssign::Append) => buffer.push_str("+="),
+                TokenType::VariableAssign(VariableAssign::Conditional) => buffer.push_str("?="),
+                TokenType::VariableAssign(VariableAssign::Bang) => buffer.push_str("!="),
+                TokenType::VariableReference => buffer.push('$'),
+                TokenType::OpenParen => buffer.push('('),
+                TokenType::CloseParen => buffer.push(')'),
+                TokenType::OpenBrace => buffer.push('{'),
+                TokenType::CloseBrace => buffer.push('}'),
+                TokenType::Colon(IsDoubleColon::No) => buffer.push(':'),
+                TokenType::Colon(IsDoubleColon::Yes) => buffer.push_str("::"),
+                TokenType::SemiColon => buffer.push(';'),
+                TokenType::Percent => buffer.push('%'),
+                TokenType::NewLine => buffer.push('\n'),
+                TokenType::CommentStart => buffer.push('#'),
+            }
+        }
+
+        buffer
+    }
+
+    proptest! {
+        #[test]
+        fn round_trip_tokens(input in r"[[:alpha:]\-=\\+!%: \t\n#$(){}¥ѨȺ🕴]+") {
+            let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+            let flat = flatten_stream(&input, &parsed);
+
+            prop_assert_eq!(input, flat);
+        }
+    }
+
+    #[test]
+    fn single_colon() {
+        let input = ":";
+        let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+
+        assert_eq!(
+            vec![Token::new(0, TokenType::Colon(IsDoubleColon::No), 1)],
+            parsed
+        );
+    }
+
+    #[test]
+    fn double_colon() {
+        let input = "::";
+        let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+
+        assert_eq!(
+            vec![Token::new(0, TokenType::Colon(IsDoubleColon::Yes), 2)],
+            parsed
+        );
+    }
+
+    #[test]
+    fn two_newlines() {
+        let input = "\n\n";
+        let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+
+        assert_eq!(
+            vec![
+                Token::new(0, TokenType::NewLine, 1),
+                Token::new(1, TokenType::NewLine, 2)
+            ],
+            parsed
+        );
+    }
+
+    #[test]
+    fn prefix_of_keyword() {
+        let input = "ife";
+        let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+
+        assert_eq!(vec![Token::new(0, TokenType::Text, 3)], parsed)
+    }
+
+    #[test]
+    fn suffix_of_keyword() {
+        let input = "ifeqa";
+        let parsed: Vec<Token> = iterator_to_token_stream(input.char_indices()).collect();
+
+        assert_eq!(vec![Token::new(0, TokenType::Text, 5)], parsed)
+    }
+}
