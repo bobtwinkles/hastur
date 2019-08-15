6f9149b289d40c421a0cde1c997a9ae73a0cf782
bobtwinkles <srkoser+github@gmail.com>
2019-08-15 04:26:50 -0400

    list_targets: More readable printing of results

--- examples/list_targets/main.rs
+++ examples/list_targets/main.rs
@@ -22,7 +22,18 @@ fn main() {
         engine.read_makefile(&mut names, &mut bufreader, &arg).expect(&format!("Failed to parse {:?}", arg));
 
         for rule in engine.database.rules() {
-            info!("{:?}", rule);
+            let target_name = names.resolve_file_name(rule.target_fname()).unwrap();
+            info!("{}", target_name);
+            let mut deps = String::new();
+            let mut fnl = "";
+            for dep in rule.dependencies() {
+                deps.push_str(fnl);
+                deps.push_str(&dep.into_string());
+
+                fnl = " ";
+            }
+
+            info!(" - {}", deps);
         }
     }
 }
