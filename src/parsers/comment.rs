c249b688204d398b3cc870e86ffa62ca352d571e
bobtwinkles <srkoser+github@gmail.com>
2019-08-15 04:27:09 -0400

    parsers/comment: clearer debug prints

--- src/parsers/comment.rs
+++ src/parsers/comment.rs
@@ -7,7 +7,6 @@ use nom::IResult;
 /// Snag all the comment content
 /// Expects to start on a '#'
 pub(super) fn parse_comment<'a>(i: BlockSpan<'a>) -> IResult<BlockSpan<'a>, (), ParseErrorKind> {
-    debug!("looking for comment in {:?}", i);
     let (mut i, mut line_parse) = preceded!(
         i,
         add_return_error!(
@@ -30,6 +29,8 @@ pub(super) fn parse_comment<'a>(i: BlockSpan<'a>) -> IResult<BlockSpan<'a>, (), 
         i = output.0;
         line_parse = output.1;
     }
+
+    debug!("New line starts at offset {:?}", i.offset);
 
     Ok((i, ()))
 }
