0270ab15672595529d153fdd93aaf00e90faa7eb
bobtwinkles <srkoser+github@gmail.com>
2019-08-17 18:10:43 -0400

    use getopts in list_targets example
    This allows us to support something like Make's `-C` options

--- examples/list_targets/main.rs
+++ examples/list_targets/main.rs
@@ -1,25 +1,37 @@
 #[macro_use]
 extern crate log;
 
-use hastur::{NameCache, Engine};
+use hastur::{Engine, NameCache};
 use std::fs::File;
 
 fn main() {
     env_logger::builder().init();
 
+    let mut args: Vec<_> = std::env::args().collect();
+
+    let mut opts = getopts::Options::new();
+    opts.optopt("C", "", "Directory to treat as the root", "DIRECTORY");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(o) => o,
+        Err(e) => panic!(e.to_string()),
+    };
+
     let mut names = NameCache::default();
 
-    let mut args = std::env::args();
-    // First argument is the binary itself, don't process that
-    args.next();
-
-    for arg in args {
+    for arg in matches.free.iter() {
         let mut engine = Engine::default();
+
+        if let Some(root) = matches.opt_str("C") {
+            engine.working_directory = root.into();
+        }
 
         let inf = File::open(&arg).expect(&format!("Failed to open file {:?}", arg));
         let mut bufreader = std::io::BufReader::new(inf);
 
-        engine.read_makefile(&mut names, &mut bufreader, &arg).expect(&format!("Failed to parse {:?}", arg));
+        engine
+            .read_makefile(&mut names, &mut bufreader, &arg)
+            .expect(&format!("Failed to parse {:?}", arg));
 
         for rule in engine.database.rules() {
             let target_name = names.resolve_file_name(rule.target_fname()).unwrap();
